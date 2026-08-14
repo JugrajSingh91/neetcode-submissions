14c6a4f9908a77048387edf3f347bb691c9d8608
JugrajSingh91 <jugraj.91@gmail.com>
2026-08-14 11:05:44 -0400

    Add: house-robber-ii - submission-16

---
+++ Data Structures & Algorithms/house-robber-ii/submission-16.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n==1) return nums[0];
+        if (n==2) return Math.max(nums[0], nums[1]);
+        
+        //[4] bottom up DP optimal
+
+        //robFirst (houses 0 to n-2)
+        int r1 = nums[0];
+        int r2 = Math.max(nums[0], nums[1]);
+
+        
+        for(int i = 2; i < n-1; i++) {
+            int robI = r1 + nums[i];
+            int skipI = r2;
+            int robFirst = Math.max(robI, skipI);
+            r1= r2;
+            r2 = robFirst;
+        }
+
+        //skipFirst (houses 1 to n-1)
+        int s1 = nums[1];
+        int s2 = Math.max(nums[1], nums[2]);
+
+        for (int i = 3; i < n; i++){
+            int robI = s1 + nums[i];
+            int skipI = s2;
+            int skipFirst = Math.max(robI, skipI);
+            s1 = s2;
+            s2 = skipFirst;
+        }
+
+        return Math.max(r2, s2); //Note S2,R2 instead of robFirst, SkipFirst
+    }
+}
